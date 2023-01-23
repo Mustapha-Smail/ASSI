8cf89e88ff9e3156460ff07bd214ad495ca73b46
Mustapha SMAIL <mustapha.smail@parisnanterre.fr>
2023-01-23 14:01:59 +0100

    initial commit

---
+++ src/main/java/projet/resources/HelloWorld.java
@@ -0,0 +1,31 @@
+package projet.resources;
+
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Request;
+import javax.ws.rs.core.UriInfo;
+
+
+@Path("/hello")
+public class HelloWorld {
+
+	@GET
+	public String sayHello() {
+		return "Hello World!"; 
+	}
+	
+	
+}
